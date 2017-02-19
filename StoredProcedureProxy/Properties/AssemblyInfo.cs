6875b8d9e9a52101483b309a0be477c74c42e1d5
Giacomo Tomolillo <giacomo.tomolillo@gmail.com>
2017-02-19 20:19:14 +0000

    Properties review

--- StoredProcedureProxy/Properties/AssemblyInfo.cs
+++ StoredProcedureProxy/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StoredProcedureProxy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Auto generate stored procedure proxies based on defined interfaces")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Giacomo Tomolillo")]
 [assembly: AssemblyProduct("StoredProcedureProxy")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
